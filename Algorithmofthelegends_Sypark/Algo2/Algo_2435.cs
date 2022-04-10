7abf09d1c0ccae1e1050862f494bbee9063c2553
ParkSoonyoung <gmb7000@naver.com>
2022-04-10 16:44:01 +0900

    Commit Algo2 2435
    2435 updates

---
+++ Algorithmofthelegends_Sypark/Algo2/Algo_2435.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+
+namespace Algo2
+{
+
+    class Algo_2435
+    {
+
+        static void Main(string[] args)
+        {
+
+            int N, K;
+            int []ResultSum;
+            int[] temp;
+            int max = -9999999;
+            string s = Console.ReadLine();
+            string[] ss = s.Split();
+
+
+            N = int.Parse(ss[0]);
+            K = int.Parse(ss[1]);
+
+            ResultSum = new int[N - K + 1];
+            System.Array.Clear(ResultSum, 0, ResultSum.Length);
+            temp = new int[N];
+
+
+            s = Console.ReadLine();
+            ss = s.Split();
+
+            for(int i = 0; i < N;i++)
+            {
+                temp[i] = int.Parse(ss[i]);
+
+            }
+
+
+            for (int i = 0; i < N - K + 1; i++)
+            {
+                for (int j = i; j < i+K; j++)
+                {
+                    ResultSum[i] += temp[j];
+
+                }
+                if (ResultSum[i] > max)
+                    max = ResultSum[i];
+            }
+
+            Console.WriteLine(max);
+        }
+
+
+    }
+
+
+
+}
