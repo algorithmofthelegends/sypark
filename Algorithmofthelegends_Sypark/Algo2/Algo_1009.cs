0a3d5cd648077857a8ae82ed63ae7c6d2d03ac2f
ParkSoonyoung <gmb7000@naver.com>
2022-04-16 09:19:52 +0900

    Commit
    add 1009, 1018

---
+++ Algorithmofthelegends_Sypark/Algo2/Algo_1009.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+namespace Algo2
+{
+
+    class Algo_1076
+    {
+
+        static void Main(string[] args)
+        {
+
+            int T;
+            String Input = Console.ReadLine();
+            T = int.Parse(Input);
+            int[] temp = new int[T];
+            int[] nResult = new int[T];
+            for (int i = 0; i < T; i++)
+            {
+                string s = Console.ReadLine();
+                string[] ss = s.Split();
+                int a = int.Parse(ss[0]) % 10;
+                int b = int.Parse(ss[1]) % 4;
+
+                if (b % 4 == 0)
+                    b = 4;
+
+                int nTemp = (int)Math.Pow(a, b);
+                nResult[i] = nTemp % 10;
+                if (nResult[i] == 0)
+                    nResult[i] = 10;
+
+            }
+            for (int i = 0; i < T; i++)
+            {
+                Console.WriteLine(nResult[i]);
+            }
+
+
+        }
+
+    }
+
+
+}
---
+++ Algorithmofthelegends_Sypark/Algo2/aogo_1018.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+
+namespace Algo2
+{
+
+    class Algo_1018
+    {
+
+        static void Main(string[] args)
+        {
+
+            string s = Console.ReadLine();
+            string[] ss = s.Split();
+
+
+            int cntW = 0;
+            int cntB = 0;
+            int nMin = 99999999;
+
+            int N = int.Parse(ss[0]);
+            int M = int.Parse(ss[1]);
+            string[] Input = new string[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                Input[i] = Console.ReadLine();
+            }
+
+            for (int i = 0; i < N - 7; i++)
+            {
+
+                for (int j = 0; j < M - 7; j++)
+                {
+                    cntW = 0;
+                    cntB = 0;
+                    for (int k = i; k < i + 8; k++)
+                    {
+                        for (int p = j; p < j + 8; p++)
+                        {
+
+                            if ((k + p) % 2 == 0)
+                            {
+                                if (Input[k][p] != 'W')
+                                    cntW++;
+                                else
+                                    cntB++;
+                            }
+                            else
+                            {
+                                if (Input[k][p] != 'B')
+                                    cntW++;
+                                else
+                                    cntB++;
+
+                            }
+
+                        }
+
+
+                    }
+
+                    int nMinTemp = Math.Min(cntW, cntB);
+                    if (nMinTemp < nMin)
+                        nMin = nMinTemp;
+
+
+                }
+
+            }
+
+            Console.WriteLine(nMin);
+
+
+
+
+        }
+
+    }
+
+
+}
