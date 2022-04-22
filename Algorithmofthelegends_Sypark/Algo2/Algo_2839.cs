9e2f92bf2b772a22ab2c55d94b68f03abc34eb44
ParkSoonyoung <gmb7000@naver.com>
2022-04-22 09:44:06 +0900

    Commit
    add 2839

---
+++ Algorithmofthelegends_Sypark/Algo2/Algo_2839.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Algo2
+{
+    class Algo_2839
+    {
+
+        static void Main(string[] args)
+        {
+
+            int N = int.Parse(Console.ReadLine());
+            List<int> result = new List<int>();
+
+            int nMin = 9999999;
+
+            int temp = N / 5;
+
+            for(int i = 0; i < temp+1; i++)
+            {
+                int a, b,c;
+                a = i;
+                b = (N - (a*5)) / 3;
+                c = (N - (a*5)) % 3;
+                if (c == 0)
+                    result.Add(a + b);
+                else
+                    result.Add(-1);
+
+
+
+            }
+
+
+            for (int i = 0; i < result.Count; i++)
+            {
+                if (result[i] != -1)
+                {
+                    if (result[i] < nMin)
+                    {
+                        nMin = result[i];
+                    }
+                }
+            }
+            if (nMin == 9999999)
+                nMin = -1;
+            Console.WriteLine(nMin);
+
+        }
+
+
+    }
+}
